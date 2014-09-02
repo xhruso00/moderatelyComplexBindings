fd2d2dcf2323a5042e75f1e50569b27ba5165d81
Marek Hrusovsky <xhruso00@gmail.com>
2014-09-02 18:51:50 +0200

    initial commit

---
+++ ModeratelyComplexBinding/Combatant.h
@@ -0,0 +1,13 @@
+//
+//  Combatant.h
+//  ModeratelyComplexBinding
+//
+//  Created by Marek Hrusovsky on 02/09/14.
+//  Copyright (c) 2014 Marek Hrusovsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Combatant : NSObject
+
+@end
---
+++ ModeratelyComplexBinding/Combatant.m
@@ -0,0 +1,13 @@
+//
+//  Combatant.m
+//  ModeratelyComplexBinding
+//
+//  Created by Marek Hrusovsky on 02/09/14.
+//  Copyright (c) 2014 Marek Hrusovsky. All rights reserved.
+//
+
+#import "Combatant.h"
+
+@implementation Combatant
+
+@end
